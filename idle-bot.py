241e64a7563775c8f4cd70346348d09ec3f789b9
Cheyenne T. Greatorex <ctg@magrathea.com>
2023-01-14 16:08:11 -0500

    move servo slowly instead of all at once, various bug fixes

--- idle-bot.py
+++ idle-bot.py
@@ -9,7 +9,7 @@ from adafruit_servokit import ServoKit
 
 _TITLE = 'IDLE BOT'
 
-_VERSION = 'Version 2.1.11'
+_VERSION = 'Version 2.2.5'
 
 _TAG_STATUS = '  STATUS: '
 _TAG_INFO = '  INFO: '
@@ -89,10 +89,14 @@ class Screen:
         if error:
             self.window.addstr(self.row_prompt, len(_TAG_PROMPT), error, curses.color_pair(_COLOR_RED) | curses.A_BOLD)
 
-    def _add_help(self, help):
+    def _add_help(self, help=None):
         self.window.move(self.row_help, 0)
         self.window.clrtoeol()
         self.window.addstr(self.row_help, 0, _TAG_HELP)
+
+        if not help:
+            return
+
         start_x = len(_TAG_HELP)
         
         in_key = False
@@ -141,7 +145,6 @@ class Screen:
             time.sleep(1)
 
     def running(self, status, info=None, error=None, help=None):
-        assert help
         self.row_help = self.row_info + 3
         self.row_prompt = self.row_help + 3
         self._add_header()
@@ -149,7 +152,7 @@ class Screen:
         self._clear()
         self._add_status(status, color=_COLOR_GREEN, bold=True)
         self._add_info(info=info)
-        self._add_help(help)
+        self._add_help(help=help)
         self._add_prompt(error=error)
         self.window.refresh()
 
@@ -162,25 +165,25 @@ class Screen:
         start = int(time.time())
         allowed_keys = self._get_allowed_keys(help)
         while elapsed < sleep:
-            elapsed = int(time.time()) - start
-            info = 'Sleeping %d of %d Seconds' % (elapsed, sleep)
+            info = 'Sleeping %d of %d Seconds' % (elapsed + 1, sleep)
             self.running(status, info=info, help=help)
             key = self.get_key(timeout=100)
             if key == -1:
                 # timeout
-                continue
+                pass
             elif key not in [ord(k) for k in allowed_keys]:
                 # invalid key
                 self.running(status, info=info, error=_COMMAND_INVALID, help=help)
                 time.sleep(.5)
-                continue
             else:
                 # valid key
                 break
+            elapsed = int(time.time()) - start
         return key
 
 def bot(window):
     screen = Screen(window)
+    servo = 0
 
     while True:
         screen.main(_STATUS_STOPPED)
@@ -188,15 +191,14 @@ def bot(window):
         key = screen.get_key(timeout=500)
 
         if key == ord('r'):
-            servo = 0
-
             while True:
+                status = 'RUNNING: Servo to NEUTRAL'
+                screen.running(status)
+                move_servo(servo, servo_config[servo]['start'])
+
                 sleep = random.randrange(45, 75)
                 status = 'RUNNING: Servo NEUTRAL'
                 help = '(E)ngage, (S)top, (Q)uit'
-
-                servo_start()
-
                 key = screen.sleep_or_get_key(sleep, status, help)
 
                 if key == ord('s'):
@@ -206,12 +208,13 @@ def bot(window):
                     screen.main(_STATUS_QUITTING)
                     return
 
-                sleep = random.uniform(5, 10)
-                status = 'RUNNING: Servo ENGAGED'
-                help = '(N)eutral, (S)top, (Q)uit'
-
+                status = 'RUNNING: Servo to ENGAGED'
+                screen.running(status)
                 move_servo(servo, servo_config[servo]['engage'])
 
+                sleep = random.randrange(5, 10)
+                status = 'RUNNING: Servo ENGAGED'
+                help = '(N)eutral, (S)top, (Q)uit'
                 key = screen.sleep_or_get_key(sleep, status, help)
 
                 if key == ord('s'):
@@ -234,17 +237,33 @@ def bot(window):
 def servo_start():
     for servo, config in enumerate(servo_config):
         angle = config['start']
-        move_servo(servo, angle)
+        config['current'] = angle
+        kit.servo[servo].angle = angle
 
-def move_servo(servo, angle):
-    kit.servo[servo].angle = angle
+def move_servo(servo, end_angle):
+    current_angle = servo_config[servo]['current']
+
+    if current_angle == end_angle:
+        return
+    elif end_angle > current_angle:
+        diff = 1
+    elif end_angle < current_angle:
+        diff = -1
+
+    while current_angle != end_angle:
+        current_angle += diff
+        kit.servo[servo].angle = current_angle
+        time.sleep(.01)
+
+    servo_config[servo]['current'] = current_angle
 
 kit = ServoKit(channels=16)
 
 servo_config = [
     {
         'start':    135,
-        'engage':   80,
+        'engage':   75,
+        'current':  0,
     },
 ]
 
